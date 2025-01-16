9df75d2e044c74828c832bd5756cbfe9b76f9c6e
Chiraag Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2025-01-16 23:44:22 +0530

    added map and location api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,12 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
